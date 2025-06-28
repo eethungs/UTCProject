68610bd2a348ab121ba1eccbe192b586a510976e
lea <leeeethung@gmail.com>
2025-06-29 00:03:09 +1000

    props file

---
+++ props_filter.py
@@ -0,0 +1,23 @@
+# in UTC
+# put in excel of UTC existing props
+# put in excel of needed props
+
+
+# make dictionary for props (sofa,couch etc)? maybe
+
+# look for needed props in existing props
+
+
+# excel of full props utc has 
+
+
+
+#between shows PGF
+# put in excel of all props
+
+# look for same props accross, which shows has what props, what can be shared
+
+# list of prop, shows needed -> if sourced which show else, none
+
+# make best show run order based on each prop (make weightage for each? couch hard to move compared to lamp)
+
